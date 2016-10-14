9196466eb92d0e66b611ca575c7fd3fa97bc884e
José María Martín Luque <jmmartinluque@gmail.com>
2016-10-14 18:33:31 +0200

    Algunos gráficos en LaTeX

--- p1_eficiencia/crear_graficos.sh
+++ p1_eficiencia/crear_graficos.sh
@@ -1,16 +1,45 @@
 #!/bin/bash
+
+filename=$(basename "$1")
+filename="${filename%.*}"
+match='\documentclass{minimal}'
+insert='\usepackage[utf8]{inputenc}'
+
+cp $1 ./gnuplot/
+
+cd ./gnuplot
+
 gnuplot << EOF
 
-set terminal png size 1280,720 enhanced font 'Arial,10'
-set ylabel '{/Arial-RegularItalic tiempo (s)}'
-set xlabel '{/Arial-RegularItalic tamaño del vector}'
-set output "./gráficos/$1.png"
+set terminal epslatex standalone color colortext
+set ylabel '{tiempo (s)}'
+set xlabel '{tamaño del vector (elementos)}'
+set output "$filename.tex"
 
-plot "$1"
+plot "$filename.dat" title 'eficiencia empírica'
+
+f(x) = (19/2)*x*x - (9/2)*x - 5
+
+set output "$filename\_teorica_superpuesta.tex"
+plot "$filename.dat" title 'eficiencia empírica', f(x) title 'eficiencia teórica'
 
 f(x) = a*x*x + b*x + c
-fit f(x) "$1" via a, b, c
-set output "./gráficos/$1_ajustado.png"
-plot "$1", f(x)
+fit f(x) "$filename.dat" via a, b, c
+set output "$filename\_ajustado.tex"
+plot "$filename.dat" title 'eficiencia empírica', f(x) title 'curva ajustada'
 
 EOF
+
+gsed -i '/\documentclass{minimal}/a\\\usepackage[utf8]{inputenc}' $filename.tex
+gsed -i '/\documentclass{minimal}/a\\\usepackage[utf8]{inputenc}' $filename\_teorica_superpuesta.tex
+gsed -i '/\documentclass{minimal}/a\\\usepackage[utf8]{inputenc}' $filename\_ajustado.tex
+
+latex $filename.tex
+latex $filename\_teorica_superpuesta.tex
+latex $filename\_ajustado.tex
+dvipdf $filename.dvi
+dvipdf $filename\_teorica_superpuesta.dvi
+dvipdf $filename\_ajustado.dvi
+
+cp *.pdf ./../entrega/img
+rm ./*
