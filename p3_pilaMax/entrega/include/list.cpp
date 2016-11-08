fb6151099ce231e1564d5c48b51f80b6e931461d
Mike <miguel_lb_97@hotmail.com>
2016-11-08 01:43:23 +0100

    Añadiendo func a list y stack_max_list

--- p3_pilaMax/entrega/include/list.cpp
+++ p3_pilaMax/entrega/include/list.cpp
@@ -4,6 +4,47 @@
  *
  */
 
+int size() const
+{
+    int n = 0;
+    for (Node<T>* aux = head; aux; aux = aux->next, ++n);
+    return n;
+}
+
+/* _________________________________________________________________________ */
+
+void push_back(const T& e)
+{
+    Node<T>* aux = new Node<T>;
+    aux->element = e;
+    tail->next = aux;
+    tail = aux;
+}
+
+/* _________________________________________________________________________ */
+
+void pop_back()
+{
+    for (Node<T>* i = head; i->next != tail; i = i->next;)
+    delete tail;
+    tail = i;
+    i->next = 0;
+}
+
+/* _________________________________________________________________________ */
+
+void clear()
+{
+    Node<T>* i = head;
+    while (i)
+    {
+        Node<T>* aux = i->next;
+        delete i;
+        i = aux;
+    }
+    head = tail = 0;
+}
+
 // No se incluye el .hpp
 
 /* Fin fichero: list.cpp */
--- p3_pilaMax/entrega/include/list.hpp
+++ p3_pilaMax/entrega/include/list.hpp
@@ -16,12 +16,53 @@
  */
 
 template <class T>
-class List {
+class List
+{
   private:
     Node<T>* head;
     Node<T>* tail;
+
   public:
     List() : head(0), tail(0) {}
+
+    /**
+     * @brief Tamaño de la lista
+     * @return Devuelve el tamaño de la lista
+     */
+    int size() const;
+
+    /**
+     * @brief Devuelve la cola de la lista
+     * @return Elemento @c T de la cola de la lista
+     */
+    T& tail() { return tail->element; }
+
+    /**
+     * @brief Devuelve la cola de la lista
+     * @return Elemento const @c T de la cola de la lista
+     */
+    const T& tail() const { return tail->element; }
+
+    /**
+     * @brief Añade un elemento al final
+     * @param node, elemento @c T que se quiere añadir
+     * @post Añade un elemento @c T en la cola de la lista
+     */
+    void push_back(const T& e);
+
+    /**
+     * @brief Elimina el ultimo
+     * @pre lista no vacia
+     * @post Elimina el ultimo elemento @c T de la lista
+     */
+    void pop_back();
+
+    /**
+     * @brief Limpia la lista entera
+     * @post Elimina la lista completamente, dejando head y tail a 0
+     */
+    void clear();
+
     // ...
 };
 
