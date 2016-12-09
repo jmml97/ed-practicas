c8b75dfae4083532e9f7a0b882cb76591d18c830
José María Martín Luque <jmmartinluque@gmail.com>
2016-12-09 23:16:18 +0100

    Filtrado por palabra clave

--- p4_STL/entrega/src/filtrado_palabra_clave.cpp
+++ p4_STL/entrega/src/filtrado_palabra_clave.cpp
@@ -23,26 +23,21 @@
   * Devuelve una cronología que contiene únicamente aquellos acontecimientos
   * de otra que contienen a su vez una palabra clave.
   */
-Cronologia& buscarPorClave (const Cronologia& c, const string& key)
+Cronologia buscarPorClave (const Cronologia& c, const string& key)
 {
-  //// ANTIGUA IMPLEMENTACIÓN; MEJORAR (CAMBIAR vector POR set)
-  /*  IMPORTANTE:
-     Si varios acontecimientos concurren el mismo año, sólo se devolverían como resultado del
-     filtrado por palabra clave aquellos relacionados con la palabra clave.
-  */
 
   Cronologia res;
   EventoHistorico tmp;
 
   for (Cronologia::const_iterator p = c.begin(); p != c.end(); ++p)
   {
-    vector<Acontecimiento> v;
-    v = p->buscarPorClave(key);
-    if (v.size() != 0)
+    std::set<Acontecimiento> s;
+    s = p->second.buscarPorClave(key);
+    if (s.size() != 0)
     {
-      tmp.setFecha(p->getFecha());
-      tmp.setEvento(v);
-      res.push_back(tmp);
+      tmp.setFecha(p->second.getFecha());
+      tmp.setEvento(s);
+      res.addEventoHistorico(tmp);
     }
   }
 
@@ -51,8 +46,40 @@ Cronologia& buscarPorClave (const Cronologia& c, const string& key)
 
 int main(int argc, char * argv[])
 {
-  // Completar programa
-  return 0;
+  if (argc != 3 && argc != 4)
+  {
+    cout << "Error: debe dar al menos el nombre de la cronología en la que quieres buscar y la palabra clave " << endl;
+    cout << "[Opcional]: un tercer nombre de fichero para guardar la cronología resultante." << endl;
+    return 1;
+   }
+
+   ifstream f1(argv[1]);
+   if (!f1)
+   {
+     cout << "No puedo abrir el fichero " << argv[1] << endl;
+     return 2;
+   }
+
+   Cronologia c1, cFiltrada;
+   f1 >> c1;
+
+   cFiltrada = buscarPorClave(c1, argv[2]);
+
+   //No se dio fichero de salida, imprimimos en salida estándar
+   if (argc == 3)
+     cout << cFiltrada;
+   else
+   {
+     ofstream fout(argv[3]);
+     if (!fout)
+     {
+       cout << "No puedo crear el fichero " << argv[3] << endl;
+       return 0;
+     }
+     fout << cFiltrada;
+   }
+
+   return 0;
 }
 
 /* Fin fichero: filtrado_palabra_clave.cpp */
